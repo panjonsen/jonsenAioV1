531893a4e2b218e4d99a0411ffa38f59f8387e98
jonsen <panjonsen@foxmail.com>
2022-05-16 18:36:56 +0800

    fix 流程

---
+++ SimplePlugin/dllmain.cpp
@@ -0,0 +1,44 @@
+﻿#include "../plugin_sdk/plugin_sdk.hpp"
+#include "Nami.h"
+#include "Lux.h"
+PLUGIN_NAME("JonSenAio");
+//SUPPORTED_CHAMPIONS(champion_id::Nami,champion_id::Lux);
+
+PLUGIN_API bool on_sdk_load(plugin_sdk_core* plugin_sdk_good)
+{
+	DECLARE_GLOBALS(plugin_sdk_good);
+	console->print("Load Script");
+
+	
+		switch (myhero->get_champion())
+	   {
+	   case champion_id::Nami:
+		   Nami::load();
+		   return true;
+	   //case champion_id::Karma:
+		  // Lux::load();
+		  // return true;
+	   default:
+		   break;
+	   }
+	//Lux::load();
+	//return true;
+	return false;
+}
+
+PLUGIN_API void on_sdk_unload()
+{
+
+
+	console->print("unload Script");
+	switch (myhero->get_champion())
+	{
+	case champion_id::Nami:
+		Nami::unload();
+		return ;
+
+	default:
+		break;
+	}
+
+}
